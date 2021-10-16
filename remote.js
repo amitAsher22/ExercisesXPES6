f87209a846532d8186ed7ea8119ec2e41e43196c
amita <amitasher4@gmail.com>
2021-10-16 17:48:32 +0300

    first commit

---
+++ remote.js
@@ -0,0 +1,55 @@
+
+// Exercise 1
+
+// Analyse the code below without running it, what will be the output ?
+
+// var num = 8;
+// var num = 10;
+// console.log(num);
+
+////// print to console : 10; ///
+
+
+////////////////////////////////////////////////////////////// Exercise 2
+
+// Exercise 2
+
+// function numbers() {
+//   for (var i = 0; i < 5; i += 1) {
+//     console.log(i);
+//   }
+//     console.log(i);
+// }
+// numbers();
+// Change the code so the var i will be undefined outside of the for loop
+
+/////// move the console.log(i) second above to - numbers();
+
+////////////////////////////////////////////////////////////// Exercise 3
+
+
+// Exercise 3
+// You have to decide which type of variables you have to use
+
+// Create a global variable that save the amount of money you have in your account
+// Create a variable that saves the amount of VAT
+// Create a variable that saves the amout of all the expenses and revenu you did /received for the past last month
+// Create a JS code that multiplies of the expenses by the VAT
+// Create a JS code that changes the amount of the money you have in your account depending on your expenses/revenu.
+// Display it
+
+
+// let amountofmoney = 10000;
+// let amountVet = amountofmoney % 17 === 0;
+
+// console.log(amountVet);
+
+// let expensesAndRevenu = 5000;
+
+// function multipliesTheExpensesVAT(){
+//     let amountAfter
+   
+// }
+
+
+
